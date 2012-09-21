0e987cfbf2e98b735dbd01810c7098dd4cdae09f
Tobias Savinainen <tcs1000@hotmail.com>
2012-09-21 15:47:24 +0200

    Added a little program that draws lines between mouse clicks

--- Xiaolin Wu's Line Algorithm/main.cpp
+++ Xiaolin Wu's Line Algorithm/main.cpp
@@ -2,14 +2,19 @@
 #include <SFML/Window/Event.hpp>
 #include "Source/XiaolinWusLineAlgorithm.hpp"
 
+#include <iostream>
+
 int main()
 {
 	sf::RenderWindow renderWindow(sf::VideoMode(800,600), "Xiaolin Wu's Line Algorithm");
 
-
+	std::vector<sf::Vector2f> points;
 
 	while(renderWindow.isOpen())
 	{
+		float x = static_cast<float>(sf::Mouse::getPosition(renderWindow).x);
+		float y = static_cast<float>(sf::Mouse::getPosition(renderWindow).y);
+
 		sf::Event event;
 		while(renderWindow.pollEvent(event))
 		{
@@ -20,13 +25,27 @@ int main()
 				if(event.key.code == sf::Keyboard::Escape)
 					renderWindow.close();
 			}
+			if(event.type == sf::Event::MouseButtonPressed)
+			{
+				if(event.mouseButton.button == sf::Mouse::Left)
+					points.push_back(sf::Vector2f(x,y));
+				if(event.mouseButton.button == sf::Mouse::Right)
+				{
+					if(!points.empty())
+						points.pop_back();
+				}
+			}
 		}
 
-		float x = sf::Mouse::getPosition(renderWindow).x;
-		float y = sf::Mouse::getPosition(renderWindow).y;
-
 		renderWindow.clear();
-		drawLine(renderWindow,400,300,x,y,sf::Color::White);
+		
+		for(std::size_t i = 0; i < points.size() - 1 && points.size() > 1; ++i)
+			drawLine(renderWindow, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, sf::Color::White);
+
+		if(!points.empty())
+			drawLine(renderWindow, points[points.size() - 1].x, points[points.size() - 1].y, x , y, sf::Color::White);
+
+		std::cout << points.size() << std::endl;
 		renderWindow.display();
 	}
 
