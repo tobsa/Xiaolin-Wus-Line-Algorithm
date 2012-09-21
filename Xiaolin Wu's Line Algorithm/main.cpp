a18d391af6cc8ec3596571ee2a1782a35c18e5fc
Tobias Savinainen <tcs1000@hotmail.com>
2012-09-21 16:26:22 +0200

    Added layer support

--- Xiaolin Wu's Line Algorithm/main.cpp
+++ Xiaolin Wu's Line Algorithm/main.cpp
@@ -5,8 +5,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////////////////////////
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics.hpp>
 #include "Source/XiaolinWusLineAlgorithm.hpp"
 #include <iostream>
 
@@ -18,6 +17,7 @@ int main()
 	sf::RenderWindow renderWindow(sf::VideoMode(800,600), "Xiaolin Wu's Line Algorithm");
 
 	std::vector<sf::Vector2f> points;
+	std::vector<std::shared_ptr<sf::RenderTexture>> textures;
 
 	while(renderWindow.isOpen())
 	{
@@ -35,16 +35,43 @@ int main()
 					renderWindow.close();
 				if(event.key.code == sf::Keyboard::Space)
 				{
+					// Don't add a new layer unless there's some points in the current layer
+					if(!points.empty())
+					{
+						// Create a new layer
+						textures.push_back(std::shared_ptr<sf::RenderTexture>(new sf::RenderTexture()));
+						int index = textures.size() - 1;
+						textures[index]->create(renderWindow.getSize().x, renderWindow.getSize().y);
 
+						// Add the last point to the current layer
+						points.push_back(sf::Vector2f(x, y));
+
+						// Draw all current points to the new layer
+						for(std::size_t i = 0; i < points.size() - 1; ++i)
+							drawLine(*textures[index], points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, sf::Color::White);
+
+						textures[index]->display();
+						points.clear();
+					}
 				}
 			}
+
 			if(event.type == sf::Event::MouseButtonPressed)
 			{
+				// Add a point to the current layer
 				if(event.mouseButton.button == sf::Mouse::Left)
 					points.push_back(sf::Vector2f(x,y));
 				if(event.mouseButton.button == sf::Mouse::Right)
 				{
-					if(!points.empty())
+					// Remove a point from the current layer
+					if(points.empty())
+					{
+						// If there's no points left on the current layer
+						// then start removing layers
+						if(!textures.empty())
+							textures.pop_back();
+					}
+					else
 						points.pop_back();
 				}
 			}
@@ -52,13 +79,17 @@ int main()
 
 		renderWindow.clear();
 		
+		// Render all the layers
+		for(std::size_t i = 0; i < textures.size(); ++i)
+			renderWindow.draw(sf::Sprite(textures[i]->getTexture()));
+
+		// Render all points from the current layer
 		for(std::size_t i = 0; i < points.size() - 1 && points.size() > 1; ++i)
 			drawLine(renderWindow, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, sf::Color::White);
 
+		// Render a line between the last point of the current layer to the mouse position
 		if(!points.empty())
 			drawLine(renderWindow, points[points.size() - 1].x, points[points.size() - 1].y, x , y, sf::Color::White);
-
-		std::cout << points.size() << std::endl;
 
 		renderWindow.display();
 	}
