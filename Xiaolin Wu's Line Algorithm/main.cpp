c0482e0c6b651b32983d51430356d762627ae9e6
Tobias Savinainen <tcs1000@hotmail.com>
2012-09-21 13:41:22 +0200

    Added  SFML window

--- Xiaolin Wu's Line Algorithm/main.cpp
+++ Xiaolin Wu's Line Algorithm/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 
 int main()
 {
+	sf::RenderWindow renderWindow(sf::VideoMode(800,600), "Xiaolin Wu's Line Algorithm");
+
+	while(renderWindow.isOpen())
+	{
+		sf::Event event;
+		while(renderWindow.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+				renderWindow.close();
+			if(event.type == sf::Event::KeyPressed)
+			{
+				if(event.key.code == sf::Keyboard::Escape)
+					renderWindow.close();
+			}
+		}
+
+		renderWindow.clear();
+		renderWindow.display();
+	}
+
 	return 0;
 }
